c3443e654db1531d22bfb1b0dc516249ccf5a61b
Mariabel17 <maribelml17@gmail.com>
2025-12-17 12:29:16 +0000

    añadidos métodos

--- src/js/components/ToDo.jsx
+++ src/js/components/ToDo.jsx
@@ -1,34 +1,85 @@
 import { useEffect, useState } from "react"
 
-function ToDo (){
+function ToDo() {
     const [list, setList] = useState([])
 
     async function getData() {
-        let response = await fetch("https://playground.4geeks.com/todo/users/maria")
-        let data = await response.json()
-        setList(data.todos)
+
+        try {
+            let response = await fetch("https://playground.4geeks.com/todo/users/marin")
+            let data = await response.json()
+            setList(data.todos)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
-    // useEffect(()=> {   //Muestra las tareas en cada recarga sin necesidad del button
-    //     getData()
-    // }, [])
+    async function postTask(event) {
+		event.preventDefault()
 
-console.log(list)
+		const newTask = {
+			"label": input,
+			"is_done": false
+		}
 
-    return(
+        await fetch("https://playground.4geeks.com/todo/users/marin", {
+			method: "POST", 
+			body: JSON.stringify(newTask), 
+			headers: { "Content-type": "application/json" } 
+		})
+
+    getData()
+
+	}
+
+    async function putTask(element) {
+        let task = {
+            "label": element.label,
+            "is_done": true
+        }
+        await fetch(`https://playground.4geeks.com/todo/users/marin${element.id}`, {
+            method: "PUT",
+            body: JSON.stringify(task),
+            headers: { "Content-type": "application/json" }
+        })
+    getData()
+
+    }
+
+
+    async function deleteTask(id) {
+        await fetch(`https://playground.4geeks.com/todo/users/marin${id}`, {
+            method: "DELETE",
+        })
+
+    }
+
+    useEffect(()=> {   //Muestra las tareas en cada recarga sin necesidad del button
+        getData()
+    }, [])
+
+    console.log(list)
+
+    return (
 
         <div className="text-center">
 
-        
-            <button onClick={()=>(getData())}>
+
+            <button onClick={() => (getData())}>
                 Traer la info
             </button>
-             {
-                list.map((e, i)=> {
+            {
+                list.map((e, i) => {
                     return (
-
                         <li key={i}>
                             {e.label}
+                            <input 
+                        type="text"
+                        value={list}
+                        onChange={(e) => setInput(e.target.value)} 
+                        placeholder="Preparar la comida">
+                        </input>
+                            <button type="button" onClick={() => (deleteTask(e.id))}>X</button>
                         </li>
 
                     )
